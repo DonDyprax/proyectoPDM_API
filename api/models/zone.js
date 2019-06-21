aeecee06d57011904b3b59d5ccca4546d9dc2de4
dondyprax <juan.acosta7a@gmail.com>
2019-06-21 14:51:26 -0600

    Cambiando el array

--- api/models/zone.js
+++ api/models/zone.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const zoneSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: String,
-    arrayLat: [{type: String}],
-    arrayLng: [{type: String}],
+    arrayLat: {type: String},
+    arrayLng: {type: String},
     building: Number,
     level: Number
 });
