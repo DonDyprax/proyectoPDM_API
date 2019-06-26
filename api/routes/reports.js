2ef6884aacee414a863502211d9ae091971b80d5
dondyprax <juan.acosta7a@gmail.com>
2019-06-25 19:07:53 -0600

    Arreglando delete y edit

--- api/routes/reports.js
+++ api/routes/reports.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 //const checkAuth = require('../middleware/check-auth');
 
+const Report = require('../models/report')
+
 const ReportsController = require('../controllers/reports');
 
 //router.get('/', checkAuth, ReportsController.reports_get_all);
@@ -15,5 +17,13 @@ router.post('/', ReportsController.reports_create_report);
 router.get('/:reportId', ReportsController.reports_get_report);
 
 router.get('delete/:reportId', ReportsController.reports_delete_report);
+
+router.get('edit/:reportId', (req, res, next) => {
+    const {id} = req.params.reportId
+    const report = Report.findById(id);
+    res.render('edit', {
+        report
+    });
+});
 
 module.exports = router;
