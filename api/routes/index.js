44ad4c4c023b5de10c3a8569f43821bf38368f9c
dondyprax <juan.acosta7a@gmail.com>
2019-06-26 00:07:07 -0600

    Casi finalizando

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const Report = require('../models/report')
+const Zone = require('../models/zone')
+
 const ReportsController = require('../controllers/reports')
 
 router.get('/',  async (req, res, next) => {
     const reports = await Report.find()
+    const zones = await Zone.find()
     res.render('index', {
-        reports
+        reports,
+        zones
     });
 });
 
