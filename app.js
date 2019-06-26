b1d17144f9b619dd550a45bcadcb7c16116ca006
dondyprax <juan.acosta7a@gmail.com>
2019-06-26 00:42:18 -0600

    Cambiando body-parser

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ mongoose.connect('mongodb+srv://dondyprax:hola100@proyectopdm-i7mvd.mongodb.net/
 
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
